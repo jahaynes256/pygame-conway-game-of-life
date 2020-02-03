9f6da50adc31053be4649f02643c09d57e86f1d4
James Haynes <jahaynes256@gmail.com>
2020-02-02 21:27:02 -0500

    Bug, New Living Cells affecting old cells

--- conway.py
+++ conway.py
@@ -2,32 +2,32 @@ import pygame, random, time, math
 
 
 #Global Variables
-rows = 125
-cols = 125
-cellSize = 4
-initialPop = 0.07
+ROWS = 125
+COLS = 125
+CELL_SIZE = 4
+INITIAL_POP = 0.07
 
 def placeIMG(IMG, x, y):
-    screen.blit(IMG, (x*cellSize, y*cellSize))
+    screen.blit(IMG, (x*CELL_SIZE, y*CELL_SIZE))
 
 def randMatrix():
-        return [[random.random() < initialPop for j in range(cols)] for i in range(rows)]
+        return [[random.random() < INITIAL_POP for j in range(COLS)] for i in range(ROWS)]
 
 def getNeighbors(cellAlive,x,y):
     count = 0
     for j in range((y-1),(y+2)):
         for i in range((x-1),(x+2)):
             if not ((i == x) and (j == y)):
-                if cellAlive[i%cols][j%rows]:
+                if cellAlive[i%COLS][j%ROWS]:
                     count += 1
     return count
 
 def getNeighborsNoWrap(cellAlive,x,y):
     count = 0
     minY=(y-1) if (y-1)>0 else 0
-    maxY=(y+2) if (y+2)<rows else rows
+    maxY=(y+2) if (y+2)<ROWS else ROWS
     minX=(x-1) if (x-1)>0 else 0
-    maxX=(x+2) if (x+2)<cols else cols
+    maxX=(x+2) if (x+2)<COLS else COLS
 
     for j in range(minY,maxY):
         for i in range(minX,maxX):
@@ -37,8 +37,8 @@ def getNeighborsNoWrap(cellAlive,x,y):
     return count
 
 def checkForStateChange(cellAlive):
-    for j in range(rows):
-        for i in range(cols):
+    for j in range(ROWS):
+        for i in range(COLS):
             n = getNeighborsNoWrap(cellAlive,i,j)
             if cellAlive[i][j]:
                 if not (2 <= n <= 3):
@@ -49,8 +49,8 @@ def checkForStateChange(cellAlive):
 
 
 def drawCells():
-    for y in range(0,rows,1):
-        for x in range(0,cols,1):
+    for y in range(0,ROWS,1):
+        for x in range(0,COLS,1):
             if cellAlive[x][y]:
                 placeIMG(aliveIMG, x, y)
             else:
@@ -60,11 +60,12 @@ def drawCells():
 pygame.init()
 
 #Create the Screen
-screen = pygame.display.set_mode((cols*cellSize,rows*cellSize))
+screen = pygame.display.set_mode((COLS*CELL_SIZE,ROWS*CELL_SIZE))
 
 #Title and Icon
 pygame.display.set_caption("Conway's Game of Life")
 icon = pygame.image.load("res\\alive_8.png")
+
 pygame.display.set_icon(icon)
 
 #Sprites
